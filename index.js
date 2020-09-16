bd390206f9dd011ac5179438d922cc3016144b59
Mauricio Carnieletto <mauriciocarnieletto@brq.com>
2020-09-16 09:54:58 -0300

    Separa o arquivo de páginas

--- index.js
+++ index.js
@@ -2,38 +2,9 @@ const fs = require("fs");
 const lighthouse = require("lighthouse");
 const chromeLauncher = require("chrome-launcher");
 const { Parser } = require("json2csv");
+const pages = require("./pages");
 
 const outputs = ["html"];
-const pages = [
-  {
-    report: "page-comparasion",
-    page: "next_home_mobile",
-    url: "https://next.me",
-    device: "mobile",
-    // network: "",
-  },
-  {
-    report: "page-comparasion",
-    page: "next_home_desktop",
-    url: "https://next.me",
-    device: "desktop",
-    // network: "",
-  },
-  {
-    report: "page-comparasion",
-    page: "next_home_desktop_4G",
-    url: "https://next.me",
-    device: "desktop",
-    // network: "",
-  },
-  {
-    report: "page-comparasion",
-    page: "next_home_mobile_4G",
-    url: "https://next.me",
-    device: "mobile",
-    // network: "",
-  },
-];
 
 async function main() {
   const reportDirectory = getTodaysReportFolder();
---
+++ pages.js
@@ -0,0 +1,32 @@
+const pages = [
+  {
+    report: "page-comparasion",
+    page: "next_home_mobile",
+    url: "https://next.me",
+    device: "mobile",
+    // network: "",
+  },
+  {
+    report: "page-comparasion",
+    page: "next_home_desktop",
+    url: "https://next.me",
+    device: "desktop",
+    // network: "",
+  },
+  {
+    report: "page-comparasion",
+    page: "next_home_desktop_4G",
+    url: "https://next.me",
+    device: "desktop",
+    // network: "",
+  },
+  {
+    report: "page-comparasion",
+    page: "next_home_mobile_4G",
+    url: "https://next.me",
+    device: "mobile",
+    // network: "",
+  },
+];
+
+module.exports = pages;
