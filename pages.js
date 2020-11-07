7fe22856bb446b1d768a552e761a7db503f6e81f
Maurício Carnieletto <mauriciocarnieletto@brq.com>
2020-11-07 20:27:52 -0300

    Analises pre deploy das melhorias

--- pages.js
+++ pages.js
@@ -1,46 +1,97 @@
 const pages = [
+
   {
     report: "page-comparasion",
-    page: "next_home_mobile",
-    url: "https://next.me",
+    page: "next_investimentose_mobile",
+    url: "https://next.me/investimentos",
     device: "mobile",
     network: "mobileSlow4G",
   },
   {
     report: "page-comparasion",
-    page: "next_home_desktop",
-    url: "https://next.me",
+    page: "next_investimentos_desktop",
+    url: "https://next.me/investimentos",
     device: "desktop",
     network: "mobileSlow4G",
   },
   {
     report: "page-comparasion",
-    page: "next_home_desktop_4G",
-    url: "https://next.me",
+    page: "next_investimentossktop_4G",
+    url: "https://next.me/investimentos",
     device: "desktop",
     network: "mobileRegular3G",
   },
   {
     report: "page-comparasion",
-    page: "next_home_mobile_4G",
-    url: "https://next.me",
+    page: "next_investimentosobile_4G",
+    url: "https://next.me/investimentos",
     device: "mobile",
     network: "mobileRegular3G",
   },
   {
     report: "page-comparasion",
-    page: "next_home_desktop_4G",
-    url: "https://next.me",
+    page: "next_investimentossktop_4G",
+    url: "https://next.me/investimentos",
     device: "desktop",
     network: "desktopDense4G",
   },
   {
     report: "page-comparasion",
-    page: "next_home_mobile_4G",
-    url: "https://next.me",
+    page: "next_investimentosobile_4G",
+    url: "https://next.me/investimentos",
     device: "mobile",
     network: "desktopDense4G",
   },
+
+
+
+
+
+
+
+  {
+    report: "page-comparasion",
+    page: "next_duvidas_mobile",
+    url: "https://next.me/duvidas",
+    device: "mobile",
+    network: "mobileSlow4G",
+  },
+  {
+    report: "page-comparasion",
+    page: "next_duvidas_desktop",
+    url: "https://next.me/duvidas",
+    device: "desktop",
+    network: "mobileSlow4G",
+  },
+  {
+    report: "page-comparasion",
+    page: "next_duvidas_desktop_4G",
+    url: "https://next.me/duvidas",
+    device: "desktop",
+    network: "mobileRegular3G",
+  },
+  {
+    report: "page-comparasion",
+    page: "next_duvidas_mobile_4G",
+    url: "https://next.me/duvidas",
+    device: "mobile",
+    network: "mobileRegular3G",
+  },
+  {
+    report: "page-comparasion",
+    page: "next_duvidas_desktop_4G",
+    url: "https://next.me/duvidas",
+    device: "desktop",
+    network: "desktopDense4G",
+  },
+  {
+    report: "page-comparasion",
+    page: "next_duvidas_mobile_4G",
+    url: "https://next.me/duvidas",
+    device: "mobile",
+    network: "desktopDense4G",
+  },
+
 ];
 
 module.exports = pages;
